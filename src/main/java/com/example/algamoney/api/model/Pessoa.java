fc5fee23c063370cb5221bf6d7783fd0dff032cf
Crysthian Pires <105651883+qwa-crysthianpires@users.noreply.github.com>
2023-01-03 20:47:37 -0300

    Modulo 4
    Removendo pessoa com DELETE
    Atualizando pessoa com PUT
    Implementando atualização parcial com PUT

--- src/main/java/com/example/algamoney/api/model/Pessoa.java
+++ src/main/java/com/example/algamoney/api/model/Pessoa.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "pessoa")
@@ -18,14 +17,13 @@ public class Pessoa {
 	private Long codigo;
 
 	@NotNull
-	@Size(min = 3, max = 20)
 	private String nome;
 
 	@Embedded
-	public Endereco endereco;
-	
+	private Endereco endereco;
+
 	@NotNull
-	private boolean ativo;
+	private Boolean ativo;
 
 	public Long getCodigo() {
 		return codigo;
@@ -43,43 +41,46 @@ public class Pessoa {
 		this.nome = nome;
 	}
 
-	public boolean isAtivo() {
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public Boolean getAtivo() {
 		return ativo;
 	}
 
-	public void setAtivo(boolean ativo) {
+	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
 	}
 
 	@Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Pessoa other = (Pessoa) obj;
-        if (codigo == null) {
-            if (other.codigo != null)
-                return false;
-        } else if (!codigo.equals(other.codigo))
-            return false;
-        return true;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
 
 	@Override
-	public String toString() {
-		return "Pessoa [codigo=" + codigo + ", nome=" + nome + ", endereco=" + endereco + ", ativo=" + ativo + "]";
-	}     
-
-    
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
 }
+
